4dacb35442e1f48bd25eac5f19b2cb31df140d95
Neginheidarii <neginheidari@icloud.com>
2023-09-24 21:49:21 -0400

    added lab 3 files

---
+++ week03_exercise 3/index.js
@@ -0,0 +1,48 @@
+var http = require("http");
+// TODO - Use Employee Module here
+const Employee = require("./employee"); // Import your Employee module
+console.log("Lab 03 - NodeJs");
+
+// Define Server Port
+const port = process.env.PORT || 3000;
+
+// Create Web Server using CORE API
+const server = http.createServer((req, res) => {
+  if (req.method !== "GET") {
+    res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+  } else {
+    if (req.url === "/") {
+      // TODO - Display message "<h1>Welcome to Lab Exercise 03</h1>"
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Welcome to Lab Exercise 03</h1>");
+    }
+
+    if (req.url === "/employee") {
+      // TODO - Display all details for employees in JSON format
+      const employees = Employee.getAllEmployees(); // Implement this function in your Employee module
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(employees));
+    }
+
+    if (req.url === "/employee/names") {
+      // TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
+      // e.g., [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
+      const employeeNames = Employee.getEmployeeNames(); // Implement this function in your Employee module
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(employeeNames));
+    }
+
+    if (req.url === "/employee/totalsalary") {
+      // TODO - Display Sum of all employees' salary in given JSON format
+      // e.g., { "total_salary": 100 }
+      const totalSalary = Employee.getTotalSalary(); // Implement this function in your Employee module
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ total_salary: totalSalary }));
+    }
+  }
+  res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
